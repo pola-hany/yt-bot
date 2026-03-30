717ca37f4f87dc0e5fabc83d4e997827b33fbfd3
pola-hany <polahany3804@gmail.com>
2026-03-30 18:14:33 +0200

    Update ytdlp_service.py

--- services/ytdlp_service.py
+++ services/ytdlp_service.py
@@ -1,7 +1,27 @@
 import yt_dlp
+import os
+import base64
+
+# فك الكوكيز لو موجودة
+def load_cookies():
+    cookies_b64 = os.getenv("COOKIES_B64")
+    
+    if cookies_b64:
+        try:
+            with open("cookies.txt", "wb") as f:
+                f.write(base64.b64decode(cookies_b64))
+            return "cookies.txt"
+        except Exception as e:
+            print("Cookie decode error:", e)
+    
+    return None
+
 
 def download_media(url, media_type, quality):
     try:
+        cookie_file = load_cookies()
+
+        # اختيار الجودة
         if media_type == "audio":
             ydl_opts = {
                 'format': 'bestaudio/best',
@@ -22,10 +42,14 @@ def download_media(url, media_type, quality):
                 'outtmpl': 'video.%(ext)s',
             }
 
+        # 👇 إضافة الكوكيز لو موجودة
+        if cookie_file:
+            ydl_opts['cookiefile'] = cookie_file
+
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=True)
             return ydl.prepare_filename(info)
 
     except Exception as e:
-        print("ERROR:", e)
+        print("DOWNLOAD ERROR:", e)
         return None
