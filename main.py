8800af4602c315fef6b0298dc64eefda415e257f
pola-hany <polahany3804@gmail.com>
2026-03-30 18:18:25 +0200

    Implement error handler for polling
    Added an error handler to log errors during polling.

--- main.py
+++ main.py
@@ -8,9 +8,16 @@ from handlers.quality import choose_quality
 
 app = ApplicationBuilder().token(TOKEN).build()
 
+# handlers
 app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_link))
 app.add_handler(CallbackQueryHandler(choose_type, pattern="video|audio"))
 app.add_handler(CallbackQueryHandler(choose_quality, pattern="144|360|720|best"))
+
+# 💥 error handler
+async def error_handler(update, context):
+    print("ERROR:", context.error)
+
+app.add_error_handler(error_handler)
 
 app.run_polling()
