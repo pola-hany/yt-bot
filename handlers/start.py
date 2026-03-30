81c358fb12e7ad4e8ff31d9973a9c00eb9bfa790
pola-hany <polahany3804@gmail.com>
2026-03-30 19:42:57 +0200

    Delete handlers directory

--- handlers/start.py
+++
@@ -1,9 +0,0 @@
-from telegram import Update
-from telegram.ext import ContextTypes
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        "🔥 أهلاً بيك!\n\n"
-        "ابعت لينك يوتيوب وأنا هخليك تختار:\n"
-        "🎥 فيديو\n🎧 صوت\nوبعدين الجودة 😏"
-    )
